9a8d879728fbae447fd3ab9789ad786b67a830e8
vimalrajsiwan <vimal.raj.siwan.se@gmail.com>
2018-12-02 16:57:06 +0530

    initial commit of project

---
+++ app/component/data/data.routing.ts
@@ -0,0 +1,25 @@
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { DataComponent } from "./data.component";
+import { AuthGuard } from "src/app/service/guard/auth.guard";
+
+
+const routes: Routes = [
+  { path: 'data', component: DataComponent,canActivate:[AuthGuard] },
+];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+//export class DataRoutingModule { }
+export const DataRoutingModule: ModuleWithProviders = RouterModule.forChild(routes);
+// @NgModule({
+//   imports: [
+//     RouterModule.forChild(routes)
+//   ],
+//   exports: [
+//     RouterModule
+//   ]
+// })
+// export class DataRoutingModule { }
